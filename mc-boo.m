96dd940554b82b9b332d0eee56cd282e734210d7
puerco-errante <uluviano@sissa.it>
2019-04-15 19:14:40 +0200

    corrected filename for plotter

--- mc-boo.m
+++ mc-boo.m
@@ -310,7 +310,7 @@ data= Get[filename];
 numDims=Length[data[[1,2]]];
 exact=Join[{1},deltaFree[numDims-1][[;;,1]]];
 Export[filename<>"rel-error"<>".pdf",ListPlot[Table[(data[[All,2]][[All,i]]-exact[[i]])/exact[[i]],{i,1,numDims}],Joined->True,GridLines->Automatic,PlotStyle->Thin,PlotLegends->Join[{"ext"},deltaFree[numDims-1][[;;,2]]],PlotLabel->"Relative Error"]];
-Export[filename<>"zoomed-rel-error"<>".pdf",ListPlot[Table[(data[[All,2]][[All,i]]-exact[[i]])/exact[[i]],{i,1,numDims}],Joined->True,GridLines->Automatic,PlotStyle->Thin,PlotRange->{-1/10,1/10},PlotLegends->Join[{"ext"},deltaFree[numDims-1][[;;,2]]],PlotLabel->"Zoomed relative error"]];
+Export[filename<>"zoomed-error"<>".pdf",ListPlot[Table[(data[[All,2]][[All,i]]-exact[[i]])/exact[[i]],{i,1,numDims}],Joined->True,GridLines->Automatic,PlotStyle->Thin,PlotRange->{-1/10,1/10},PlotLegends->Join[{"ext"},deltaFree[numDims-1][[;;,2]]],PlotLabel->"Zoomed relative error"]];
 (*{Mean[data[[-100;;-1,2]]],StandardDeviation[data[[-100;;-1,2]]],Mean[data[[-100;;-1,3]]],StandardDeviation[data[[-100;;-1,3]]]}*)
 Return[{data[[-1,2]],data[[-1,3]]}];
 ];
@@ -319,7 +319,7 @@ chi2PlotnAv[filename_,exact_]:=Block[{data,numDims},
 data= Get[filename];
 numDims=Length[data[[1,2]]];
 Export[filename<>"rel-error"<>".pdf",ListPlot[Table[(data[[All,2]][[All,i]]-exact[[i,1]])/exact[[i,1]],{i,1,numDims}],Joined->True,GridLines->Automatic,PlotStyle->Thin,PlotLegends->exact[[;;,2]],PlotLabel->"Relative Error"]];
-Export[filename<>"zoomed-rel-error"<>".pdf",ListPlot[Table[(data[[All,2]][[All,i]]-exact[[i,1]])/exact[[i,1]],{i,1,numDims}],Joined->True,GridLines->Automatic,PlotStyle->Thin,PlotRange->{-1/10,1/10},PlotLegends->exact[[;;,2]],PlotLabel->"Zoomed relative error"]];
+Export[filename<>"zoomed-error"<>".pdf",ListPlot[Table[(data[[All,2]][[All,i]]-exact[[i,1]])/exact[[i,1]],{i,1,numDims}],Joined->True,GridLines->Automatic,PlotStyle->Thin,PlotRange->{-1/10,1/10},PlotLegends->exact[[;;,2]],PlotLabel->"Zoomed relative error"]];
 (*{Mean[data[[-100;;-1,2]]],StandardDeviation[data[[-100;;-1,2]]],Mean[data[[-100;;-1,3]]],StandardDeviation[data[[-100;;-1,3]]]}*)
 Return[{data[[-1,2]],data[[-1,3]]}];
 ];
